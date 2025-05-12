25f059561eb035339dd01ae2254da7179f3f05e6
kbiswas20 <biswaskripamoye@gmail.com>
2025-05-12 18:53:45 -0400

    Added vercel.json

--- vite-project/my-react-app/vite.config.js
+++ vite-project/my-react-app/vite.config.js
@@ -1,8 +1,14 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // this is fine for Vercel
-});
+  css: {
+    postcss: {
+      plugins: [
+        require('@tailwindcss/postcss')(),
+        require('autoprefixer')(),
+      ],
+    },
+  },
+})
