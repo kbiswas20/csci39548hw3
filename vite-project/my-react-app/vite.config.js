8c0c925ccfae4c374d349f91015b8541fbfaddbc
kbiswas20 <biswaskripamoye@gmail.com>
2025-05-12 22:35:55 -0400

    Updated vercel.json

--- vite-project/my-react-app/vite.config.js
+++ vite-project/my-react-app/vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure you're deploying to root
   css: {
     postcss: {
       plugins: [
-        require('@tailwindcss/postcss')(),
+        require('tailwindcss')(),
         require('autoprefixer')(),
       ],
     },
